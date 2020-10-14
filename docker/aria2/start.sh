283f290d7d9820345360aa3ca119e4bcbbc3ae19
ksana410 <ddxiong0410@gmail.com>
2020-10-14 13:59:45 +0800

    update aria2 conf

--- docker/aria2/start.sh
+++ docker/aria2/start.sh
@@ -4,3 +4,8 @@ sessionfile="/aria2/conf/arai2.session"
 dhtfile="/aria2/conf/dht.dat"
 
 if [ ! -f "$confile" ]; then
+    echo "Error" && exit 0
+fi
+
+/usr/bin/aria2c --conf-path=$confile --dir=/aria2/downloads \
+--rpc-secret=${SECRET}
