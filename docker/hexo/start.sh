835beb6eba249d6429294de09309de9b3c5205d2
ksana410 <ddxiong0410@gmail.com>
2021-01-17 19:22:33 +0800

    add function

--- docker/hexo/start.sh
+++ docker/hexo/start.sh
@@ -3,36 +3,64 @@
 set -o errexit
 
 # check directory for hexo
-if [[ `ls -A /hexo|wc -w` -eq "0" ]]
-then
-    git clone -b $TAG $GITSITE /hexo
+checkdir() {
+    if [[ `ls -A /hexo|wc -w` -eq "0" ]]  #判断hexo目录是否为空
+    then
+        git clone -b $TAG $GITSITE /hexo
+    echo "git clone over"
+    fi
+}
+
+setgitnpm() {
     if [[ ! -z ${PROXY} ]]
     then
         npm config set proxy ${PROXY}
         npm config set https-proxy ${PROXY}
         git config --global http.proxy ${PROXY}
         git config --global https.proxy ${PROXY}
+    elif [[ ! -z ${GITUSER} && ! -z ${GITEMAIL} ]]
+    then
+        git config --gloable user.name ${GITUSER}
+        git config --gloable user.email ${GITEMAIL}
     fi
-    echo "git clone over"
-fi
-
+    touch /setgitnpm.tmp
+}
 # install hexo package
-cd /hexo/data
-npm install
+installhexo() {
+    cd /hexo
+    npm install
+    npm audit fix
+    touch /installhexo.tmp
+}
 
 # init config
-if [[ ! -f "/tmp/sshconfig.tmp" ]]
-then
-    echo "init config ..."
-    echo "root:$PASSWORD" | chpasswd
-    echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
-    echo 'Port 22' >> /etc/ssh/sshd_config
-    ssh-keygen -A
-    touch /tmp/sshconfig.tmp
-else
-    echo "ssh server will start!"
-fi
+initsshconf() {
+    if [[ ! -f "/sshconfig.tmp" ]]
+    then
+        echo "init config ..."
+        echo "root:$PASSWORD" | chpasswd
+        echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
+        echo 'Port 22' >> /etc/ssh/sshd_config
+        ssh-keygen -A
+        touch /sshconfig.tmp
+    else
+        echo "ssh config is ok!"
+    fi
+}
 
 # start ssh server
-echo "ssh server starting..."
-/usr/sbin/sshd -D
+startssh() {
+    checkdir
+    if [[ ! -f /setgitnpm.tmp ]]
+    then
+        setgitnpm
+    elif [[ ! -f /installhexo.tmp ]]
+    then
+        installhexo
+    fi
+    initsshconf
+    echo "ssh server starting..."
+    /usr/sbin/sshd -D
+}
+
+startssh
