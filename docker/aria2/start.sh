2e41e9f8429bbfe666dcf3c4563864cde1625e13
ksana410 <ddxiong0410@gmail.com>
2021-07-09 23:30:11 +0800

    File format adjustment

--- docker/aria2/start.sh
+++ docker/aria2/start.sh
@@ -3,9 +3,10 @@ confile="/aria2/conf/aria2.conf"
 sessionfile="/aria2/conf/arai2.session"
 dhtfile="/aria2/conf/dht.dat"
 
-if [ ! -f "${confile}" ]; then
+if [[ ! -f "${confile}" ]]
+then
     echo "Error" && exit 0
 fi
 
-/usr/bin/aria2c --conf-path=$confile --dir=/aria2/downloads \
+exec /usr/bin/aria2c --conf-path=${confile} --dir=/aria2/downloads \
 --rpc-secret=${SECRET}
