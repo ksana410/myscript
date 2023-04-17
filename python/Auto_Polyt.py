e58d73da16dcc18a40aed13aa29fd4492dce63db
ksana410 <ddxiong0410@gmail.com>
2023-04-17 15:59:19 +0800

    post test

--- python/Auto_Polyt.py
+++ python/Auto_Polyt.py
@@ -8,6 +8,19 @@ import re
 import userdata
 
 
+cookie = userdata.cookie
+authorization = userdata.authorization
+header = {
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36 Edg/112.0.1722.34',
+    'Referer': 'https://m.polyt.cn/',
+    'Authorization': authorization,
+    'Content-Type': 'application/json',
+    'Channel': 'plat_h5',
+    'Connection': 'keep-alive',
+    'Cookie': cookie
+}
+session = requests.Session()
+
 # 对象化编程
 # 执行流程如下
 # 1. 要求用户输入演出名，所处城市，工具会基于上述两个输入确定具体的演出项目
@@ -15,23 +28,11 @@ import userdata
 # 3. 此时对象自动将获取到的演出时间，票价，观演人, 此时可以添加观演人，最终存储用户所选，需要注意，由于实名制的要求，购票数量对应观演人数量，故指定观演人即可
 # 4. 之后软件将自动运行，直到抢购到为止
 class polytAuto:
-    cookie = userdata.Cookie
-    authorization = userdata.authorization
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36 Edg/112.0.1722.34',
-        'Referer': 'https://m.polyt.cn/',
-        'Authorization': authorization,
-        'Content-Type': 'application/json',
-        'Channel': 'plat_h5',
-        'Connection': 'keep-alive',
-        'Cookie': cookie
-    }
-    session = requests.session()
     def __init__(self, keyword: str, city: str):
         self.keyword = keyword
         self.city = city
         self.url = 'https://m.polyt.cn/platform-backend/'
-        self.session = session.headers.update(headers)
+        self.session = session.headers.update(header)
         
     # 建立get操作，如果失败则重试三次，三次重试失败后终止函数
     def _get(self, url: str):
@@ -51,7 +52,7 @@ class polytAuto:
     
     # 建立post操作，三次请求失败则退出程序
     def _post(self, url: str, json_data: dict):
-        retry_count = 3
+        retry_count = 1
         try:
             response = self.session.post(url, json = data)
             response.raise_for_status()
